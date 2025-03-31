121e379d5360e8f85a5a50905538c9fa09b60d26
olivcho <olivercho007@gmail.com>
2025-03-31 13:50:43 -0700

    add feedforward neural network functionality

--- main.py
+++ main.py
@@ -12,10 +12,32 @@ class Neuron:
         return sigmoid(total)
 
 # Example usage for 2-input neuron
-weights = [0, 1]
+weights = np.array([0, 1])
 bias = 4
 inputs = np.array([3, 4])
 
 n = Neuron(weights, bias)
-print(n.feedforward(inputs)) # 0.999664649
+print(f"2-Input Neuron Output: {n.feedforward(inputs)}") # 0.999664649
+
+class NeuralNetwork:
+    def __init__(self):
+        weights = np.array([0, 1]) # Assign all neurons the same weights
+        bias = 0 # Assign all neurons the same bias
+
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        out_h1 = self.h1.feedforward(inputs)
+        out_h2 = self.h2.feedforward(inputs)
+        out_o1 = self.o1.feedforward([out_h1, out_h2])
+
+        return out_o1
+    
+# Example usage for neural network with 1 hidden layer and 2 neurons
+inputs = np.array([2, 3])
+
+n = NeuralNetwork()
+print(f"Neuron Network Output: {n.feedforward(inputs)}")
 
