f0e31c0093997c27c823d12460c3c87800925e03
olivcho <olivercho007@gmail.com>
2025-04-01 12:32:44 -0700

    create full model but not working yet

--- main.py
+++ main.py
@@ -52,43 +52,72 @@ class NeuralNetwork:
                 w1_dydh = self.w5 * (out_o1 * (1 - out_o1))
                 w1_dhdw = x[0] * (out_h1 * (1 - out_h1))
                 w1_grad = w1_dLdy * w1_dydh * w1_dhdw
-                self.w1 -= learning_rate * (w1_grad)
                 # w2h1
+                w2_dLdy = -2 * (y - out_o1)
+                w2_dydh = self.w5 * (out_o1 * (1 - out_o1)) 
+                w2_dhdw = x[1] * (out_h1 * (1 - out_h1))
+                w2_grad = w2_dLdy * w2_dydh * w2_dhdw
                 # b1h1
+                b1_dLdy = -2 * (y - out_o1)
+                b1_dydh = self.w5 * (out_o1 * (1 - out_o1)) 
+                b1_dhdb = (out_h1 * (1 - out_h1))
+                b1_grad = b1_dLdy * b1_dydh * b1_dhdb
 
                 # w3h2
+                w3_dLdy = -2 * (y - out_o1)
+                w3_dydh = self.w6 * (out_o1 * (1 - out_o1))
+                w3_dhdw = x[0] * (out_h2 * (1 - out_h2))
+                w3_grad = w3_dLdy * w3_dydh * w3_dhdw
+
                 # w4h2
+                w4_dLdy = -2 * (y - out_o1)
+                w4_dydh = self.w6 * (out_o1 * (1 - out_o1))
+                w4_dhdw = x[1] * (out_h2 * (1 - out_h2))
+                w4_grad = w4_dLdy * w4_dydh * w4_dhdw
+
                 # b2h2
+                b2_dLdy = -2 * (y - out_o1)
+                b2_dydh = self.w6 * (out_o1 * (1 - out_o1))
+                b2_dhdb = (out_h2 * (1 - out_h2))
+                b2_grad = b2_dLdy * b2_dydh * b2_dhdb
 
                 # w5o1
+                w5_dLdy = -2 * (y - out_o1)
+                w5_dydo = out_o1 * (1 - out_o1)
+                w5_dodw = out_h1
+                w5_grad = w5_dLdy * w5_dydo * w5_dodw
+
                 # w6o1
+                w6_dLdy = -2 * (y - out_o1)
+                w6_dydo = out_o1 * (1 - out_o1)
+                w6_dodw = out_h2
+                w6_grad = w6_dLdy * w6_dydo * w6_dodw
+
                 # b3o1
+                b3_dLdy = -2 * (y - out_o1)
+                b3_dydo = out_o1 * (1 - out_o1)
+                b3_dodb = 1
+                b3_grad = b3_dLdy * b3_dydo * b3_dodb
 
-                pass
+                self.w1 -= learning_rate * w1_grad
+                self.w2 -= learning_rate * w2_grad
+                self.b1 -= learning_rate * b1_grad
+                self.w3 -= learning_rate * w3_grad
+                self.w4 -= learning_rate * w4_grad
+                self.b2 -= learning_rate * b2_grad
+                self.w5 -= learning_rate * w5_grad
+                self.w6 -= learning_rate * w6_grad
+                self.b3 -= learning_rate * b3_grad
 
-# Backprop -- change in loss function with respect to weight 1
-# dL/dw_1 = dL/dy * dy/dh_1 * dh_1/dw_1
-# Recall that y = (1-y_pred)^2 by the MSE formula for one point
-# Sigmoid: f(x) = 1/(1+exp(-x))
-# Derivative: f'(x) = f(x) * (1-f(x))
-
-# Example usage of backprop for previous NN example
-
-dldy = -2 * (1 - o1[2]) # chain rule
-dydh1 = 0.5 * (sigmoid(o1[2]) * (1 - sigmoid(o1[2]))) # chain rule on sigmoid function for o1
-dh1dw1 = inputs[0] * (sigmoid(o1[0]) * (1 - sigmoid(o1[0]))) # chain rule on sigmoid function for h1
-
-dldw = dldy * dydh1 * dh1dw1
-print(f"Backprop: {dldw}")
-
-
-
-
+                if epoch % 10 == 0:
+                    loss = MSE(labels, out_o1)
+                    print("Epoch %d loss: %.3f" % (epoch, loss))
 
 def MSE(y_true, y_pred): # y_true and y_pred are numpy arrays with the same length
     return ((y_true - y_pred) ** 2).mean()
 
-data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-labels = np.array([[0], [1], [1], [0]])
+data = np.array([[0, 3], [5, 12], [-5, 0], [1, 5]])
+labels = np.array([[0], [1], [0], [1]])
 
 nn = NeuralNetwork()
+nn.train(data, labels)
